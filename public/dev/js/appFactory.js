4cf40a3dff984ae01e4d49a90759ed6b5bb0c8b0
jj <jjdeepali@gmail.com>
2016-11-22 18:11:13 +0530

    change dlab-api to localhost

--- public/dev/js/appFactory.js
+++ public/dev/js/appFactory.js
@@ -40,7 +40,7 @@ app.factory('utilsFac', function(){
 });
 
 app.factory('endpointFac', ['$cookies', function($cookies) {
-    var urlBase = 'http://54.191.89.172:9000/';
+    var urlBase = 'http://127.0.0.1:9000/';
     return {
         url_get_issues: function(filterId) {
             return urlBase + 'api/community/issue' + '?filter_id=' + filterId;
