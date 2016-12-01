60dc4629fbfeac9f4e3c9a773d14511f432452a7
Ubuntu <ubuntu@ip-172-31-22-27.us-west-2.compute.internal>
2016-12-01 18:11:35 +0000

    added is_admin:true to create an admin user

--- public/dev/js/appController.js
+++ public/dev/js/appController.js
@@ -48,7 +48,8 @@ app.controller("main-controller", [ '$http', '$location', '$cookies', 'accessFac
                 username: self.new_user,
                 password: self.new_pass,
                 name:     self.name,
-                city:     self.city
+                city:     self.city,
+                is_admin: true
             });
             dataFac.put(endpointFac.url_post_user(), user_arg).then(function(data){utilsFac.echo(data);});
         };
